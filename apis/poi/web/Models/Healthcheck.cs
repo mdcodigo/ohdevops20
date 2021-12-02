6fb72a9c76b9d49748d8859fbe65a772fb3f0e14
mdcodigo <70071224+mdcodigo@users.noreply.github.com>
2021-12-02 15:05:52 -0300

    Agrego campo "version".

--- apis/poi/web/Models/Healthcheck.cs
+++ apis/poi/web/Models/Healthcheck.cs
@@ -10,11 +10,15 @@ namespace poi.Models
             Message = "POI Service Healthcheck";
             Status = "Healthy";
             //Status = "Broken...";
+            Version = "v15";
         }
         [Newtonsoft.Json.JsonProperty(PropertyName = "message")]
         public string Message {get;set;}
 
         [Newtonsoft.Json.JsonProperty(PropertyName = "status")]
         public string Status { get; set; }
+        
+        [Newtonsoft.Json.JsonProperty(PropertyName = "version")]
+        public string Version { get; set; }
     }
 }
